e9ad03bb6e33350a87cd6c5718542b897b47484d
Marko Strukelj <marko@nirvana.(none)>
2011-11-27 23:49:40 +0100

    Multiple @PostConstruct methods

--- src/main/java/org/jboss/as/quickstarts/helloworld/HelloService.java
+++ src/main/java/org/jboss/as/quickstarts/helloworld/HelloService.java
@@ -1,16 +1,40 @@
 package org.jboss.as.quickstarts.helloworld;
 
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
 /**
  * A simple CDI service which is able to say hello to someone
  * 
  * @author Pete Muir
  * 
  */
+@ApplicationScoped
 public class HelloService {
 
-   public HelloService()
-   {
+   public HelloService() {
       System.out.println("HelloService<init>");
+   }
+
+   @Inject
+   void init() {
+      System.out.println("Initializer method - init()");
+   }
+
+    @Inject
+    void init2() {
+       System.out.println("Initializer method 2 - init2()");
+    }
+
+   @PostConstruct
+   void postConstruct() {
+      System.out.println("@PostConstruct method");
+   }
+
+   @PostConstruct
+   void postConstruct2() {
+      System.out.println("@PostConstruct method 2");
    }
 
    String createHelloMessage(String name) {

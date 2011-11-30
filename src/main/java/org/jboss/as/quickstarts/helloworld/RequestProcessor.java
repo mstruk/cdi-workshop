8c487015266ac408e233f98aba2276cb35b1b27e
Marko Strukelj <mstrukel@redhat.com>
2011-11-30 23:50:04 +0100

    Private no-args constructor test

--- src/main/java/org/jboss/as/quickstarts/helloworld/RequestProcessor.java
+++ src/main/java/org/jboss/as/quickstarts/helloworld/RequestProcessor.java
@@ -19,7 +19,7 @@ public class RequestProcessor {
 
    @Inject @HttpParam("action") private String action2;
 
-   public RequestProcessor() {
+   private RequestProcessor() {
       System.out.println("RequestProcessor<init>");
    }
 

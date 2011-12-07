8707a9cffd6a86429c1d35e6c8041a2d09057427
Marko Strukelj <mstrukel@redhat.com>
2011-12-07 01:06:23 +0100

    Producer - lifecycle of normal scoped managed bean with producer

--- src/main/java/org/jboss/as/quickstarts/helloworld/UserNameProvider.java
+++ src/main/java/org/jboss/as/quickstarts/helloworld/UserNameProvider.java
@@ -3,12 +3,14 @@ package org.jboss.as.quickstarts.helloworld;
 import org.jboss.as.quickstarts.helloworld.annotations.LoggedIn;
 import org.jboss.as.quickstarts.helloworld.annotations.UserName;
 
+import javax.enterprise.context.RequestScoped;
 import javax.enterprise.inject.Default;
 import javax.enterprise.inject.Produces;
 
 /**
  * @author <a href="mailto:mstrukel@redhat.com>Marko Strukelj</a>
  */
+@RequestScoped
 public class UserNameProvider {
 
    public UserNameProvider() {
